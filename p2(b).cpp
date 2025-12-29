279dc29986d1722492cb09cf0512e158f8bedac8
keerthana-sasikumar <keerthanasasikumar08@gmail.com>
2025-12-29 20:19:10 +0530

    Create p2(b).cpp

---
+++ p2(b).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void reverse(int *n)
+{
+    int rev = 0, temp = *n;
+
+    while (temp > 0)
+    {
+        rev = rev * 10 + temp % 10;
+        temp /= 10;
+    }
+
+    cout << "Reversed number: " << rev << endl;
+}
+
+int main()
+{
+    int num;
+    cout << "Enter number: ";
+    cin >> num;
+
+    reverse(&num);
+    return 0;
+}
